246b7183e22c0ff4b2dca12dca6c9570a869f368
SvyatoyTapok <ewfbewrb@mail.ru>
2025-01-26 02:41:49 +0300

    done bookings + try catch

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import userRouter from './routes/users.js';
-const app = express();
+import bookingsRouter from './routes/bookings.js';
+
+const PORT = 4300
+const app = express()
 
 app.use(userRouter)
+app.use(bookingsRouter)
 
-app.listen(4300, () => {
-    console.log(`Server is started on port 4300`)
+app.listen(PORT, () => {
+    console.log(`Server is started on port ${PORT}`)
 })
--- routes/users.js
+++ routes/users.js
@@ -10,58 +10,80 @@ userRouter.post('/users', async (request, response) => {
         response.send({ ...rows[0], ...request.query })
     } catch (e) {
         response.status(409)
-        response.send(`${e.detail} `)
+        response.send(`${e.detail}`)
     }
 })
 
 //PATCH USER BY ID
-userRouter.patch('/user', async (request, response) => {
-    const { rowCount } = await db.query('UPDATE users SET first_name = $1, second_name = $2, phone = $3 WHERE id = $4',
-        [request.query.first_name, request.query.second_name, request.query.phone, request.query.id])
-    if (rowCount === 1) {
-        response.send(request.query)
-    } else {
-        response.status(409)
-        response.send('Not found this ID')
+userRouter.patch('/user/:id', async (request, response) => {
+    try {
+        const { rowCount } = await db.query('UPDATE users SET first_name = $1, second_name = $2, phone = $3 WHERE id = $4',
+            [request.query.first_name, request.query.second_name, request.query.phone, request.params.id])
+        if (rowCount === 1) {
+            response.send(request.query)
+        } else {
+            response.status(409)
+            response.send(`Not found ID = ${request.params.id}`)
+        }
+    } catch (e) {
+        response.send(e)
     }
 })
 
 //GET ONE USER BY ID
 userRouter.get('/user/:id', async (request, response) => {
-    const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [request.params.id])
-    if (rows.length === 0) {
-        response.status(404)
-        response.send(`Not found this id${rows}`)
-    } else {
-        response.send(rows[0])
+    try {
+        const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [request.params.id])
+        if (rows.length === 0) {
+            response.status(404)
+            response.send(`Not found this id${rows}`)
+        } else {
+            response.send(rows[0])
+        }
+    } catch (e) {
+        response.send(e)
     }
 })
 
 //GET ALL USERS
-userRouter.get('/users', async (request, response) => {
-    const { rows } = await db.query('SELECT * FROM users')
-    response.send(rows)
+userRouter.get('/users', async (_, response) => {
+    try {
+        const { rows } = await db.query('SELECT * FROM users')
+        response.send(rows)
+    } catch (e) {
+        response.send(e)
+    }
 })
 
 //DELETE USER BY ID
 userRouter.delete('/user/:id', async (request, response) => {
-    const dbresponse = await db.query('DELETE FROM users WHERE id = $1', [request.params.id])
-    if (dbresponse.rowCount === 1) {
-        response.status(200)
-        response.send(`Delete id ${request.query.id}`)
-    } else {
-        response.status(404)
-        response.send(`Not found id ${request.params.id}`)
+    try {
+        const dbresponse = await db.query('DELETE FROM users WHERE id = $1', [request.params.id])
+        if (dbresponse.rowCount === 1) {
+            response.status(200)
+            response.send(`Delete id ${request.query.id}`)
+        } else {
+            response.status(404)
+            response.send(`Not found id ${request.params.id}`)
+        }
+    } catch (e) {
+        response.send(e)
     }
 })
 
 //DELETE ALL USERS
-userRouter.delete('/users', async (request, response) => {
-    const { rowCount } = await db.query('DELETE from users')
-    if (rowCount === 0) {
-        response.send('Users table is already empty')
-    } else {
-        response.send('All users was deleted succesfully')
+userRouter.delete('/users', async (_, response) => {
+    try {
+        const { rowCount } = await db.query('DELETE from users')
+        if (rowCount === 0) {
+            response.send('Users table is already empty')
+        } else {
+            response.send('All users was deleted succesfully')
+        }
+    } catch (e) {
+        response.status(409)
+        response.send(e.detail)
     }
 })
+
 export default userRouter
