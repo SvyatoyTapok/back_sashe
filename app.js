ecff1ca86ae44304425643c9170ffeceee982e23
SvyatoyTapok <ewfbewrb@mail.ru>
2025-05-21 13:06:53 +0300

    frfr3

--- app.js
+++ app.js
@@ -5,17 +5,26 @@ import imagesRouter from './routes/images.js';
 import priceRouter from './routes/price.js';
 import workersRouter from './routes/workers.js';
 import cors from 'cors';
+import path from 'path';
 
 const PORT = 4300;
 const app = express();
 const __dirname = path.resolve();
 
+const allowedOrigins = ['https://sashestudio.ru', 'https://www.sashestudio.ru'];
+
 app.use(cors({
-    origin: 'https://sashestudio.ru',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.use('/images/gallery', express.static(path.join(__dirname, 'images', 'gallery'), { redirect: false })); app.use(express.json());
 
 app.use('/images', express.static('images')),
     app.use(
